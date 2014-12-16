13dbe279fc1c3905359749529e3829965b9e5d87
tony <ipkes@rambler.ru>
2014-12-17 01:47:05 +0300

    and another spec fix

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe QuestionsController, :type => :controller do
    before { get :index }
 
    it 'populates an array of all question' do
-     expect(assigns(:questions)).to match_array questions
+     expect(assigns(:questions)).to include *questions
    end
 
    it 'render index view' do
